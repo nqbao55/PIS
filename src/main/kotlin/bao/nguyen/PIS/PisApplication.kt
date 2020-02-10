fbe7972d64d83a202a38a677811c55c9da377dec
Bao Nguyen <nqbao.qn@gmail.com>
2020-02-10 09:44:45 +0700

    Init project

---
+++ src/main/kotlin/bao/nguyen/PIS/PisApplication.kt
@@ -0,0 +1,11 @@
+package bao.nguyen.PIS
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PisApplication
+
+fun main(args: Array<String>) {
+	runApplication<PisApplication>(*args)
+}
---
+++ src/test/kotlin/bao/nguyen/PIS/PisApplicationTests.kt
@@ -0,0 +1,13 @@
+package bao.nguyen.PIS
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PisApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
