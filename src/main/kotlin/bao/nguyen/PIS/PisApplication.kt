4ea6951e86c2f7f76f05e39fbc0988cde0eddaf7
Bao Nguyen <bao.nguyen@rikai.technology>
2020-03-03 14:25:19 +0700

    Update code

--- src/main/kotlin/bao/nguyen/PIS/PisApplication.kt
+++ src/main/kotlin/bao/nguyen/PIS/PisApplication.kt
@@ -1,11 +1,20 @@
 package bao.nguyen.PIS
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class PisApplication
+class PisApplication: SpringBootServletInitializer(){
+	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+		return application.sources(PisApplication::class.java)
+	}
+}
 
 fun main(args: Array<String>) {
-	runApplication<PisApplication>(*args)
+//	runApplication<PisApplication>(*args)
+	SpringApplication.run(PisApplication::class.java, *args)
 }
+
